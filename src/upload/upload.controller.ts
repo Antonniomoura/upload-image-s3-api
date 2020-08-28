d1c2afd2eb37a3e2e8047c35167c93c9b37b70c8
Antonio Moura <antonio.moura@bravi.com.br>
2020-08-27 22:56:30 -0300

    feat: configura S3

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  constructor(private uploadService: UploadService) {
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: any): Promise<any> {
+    return await this.uploadService.uploadFile(file)
+  }
+}
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+
+const AWS_S3_BUCKET_NAME = 'AWS_S3_BUCKET_NAME';
+
+const S3 = new AWS.S3({
+  accessKeyId: 'ACCESS_KEY_ID',
+  secretAccessKey: 'SECRET_ACCESS_KEY',
+});
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  async uploadFile(file: any): Promise<any> {
+    const urlKey = '';
+    const params = {
+      Body: file.buffer,
+      Bucket: AWS_S3_BUCKET_NAME,
+      Key: urlKey,
+    };
+
+    return await S3.putObject(params).promise().then(data => {
+      console.log(data);
+      return data;
+    }, error => {
+      console.log(error);
+      return error;
+    });
+  }
+}
