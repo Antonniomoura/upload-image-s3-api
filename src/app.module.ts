4f9ff6c7be01834daffb93f5345b956c31d15948
Antonio Moura <antonio.moura@bravi.com.br>
2020-08-28 13:34:56 -0300

    fix: adjustment in upload file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, ConfigModule.forRoot(
+    {
+      isGlobal: true,
+      envFilePath: '.env'
+    },
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,29 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
-
-const AWS_S3_BUCKET_NAME = 'AWS_S3_BUCKET_NAME';
-
-const S3 = new AWS.S3({
-  accessKeyId: 'ACCESS_KEY_ID',
-  secretAccessKey: 'SECRET_ACCESS_KEY',
-});
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UploadService {
+  private s3;
+
+  constructor() {
+    this.s3 = new AWS.S3({
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    });
+  }
+
   async uploadFile(file: any): Promise<any> {
-    const urlKey = '';
+    const urlKey = `imagem/${uuidv4()}-${file.originalname.replace(' ', '')}`;
+
     const params = {
       Body: file.buffer,
-      Bucket: AWS_S3_BUCKET_NAME,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: urlKey,
     };
 
-    return await S3.putObject(params).promise().then(data => {
-      console.log(data);
-      return data;
-    }, error => {
-      console.log(error);
-      return error;
+    return await this.s3.putObject(params).promise().then(() => {
+      return { urlKey: urlKey };
+    }, () => {
+      throw new BadRequestException('Error no upload do arquivo')
     });
   }
 }
